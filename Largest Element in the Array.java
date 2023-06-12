d4a44a41609a6d47c10cbc6ae0cdd9457f244bce
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-06-13 01:21:50 +0530

    Update Largest Element in the Array.java

--- Largest Element in the Array.java
+++ Largest Element in the Array.java
@@ -1,1 +1,18 @@
 
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+    static int largestElement(int[] arr, int n) {
+        // Write your code here.
+        int largest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++){
+            if (arr[i] >= largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+}
